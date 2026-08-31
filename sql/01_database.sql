16998b349c65922fbfdf844f4ee2c3ae2085676e
Shekhawat <rajputsahilshekhawat@gmail.com>
2026-08-31 16:09:48 +0530

    Initial commit

---
+++ sql/01_database.sql
@@ -0,0 +1,11 @@
+/*
+==========================================================
+Retail Sales Data Warehouse
+File: 01_database.sql
+For Aiven
+==========================================================
+*/
+
+USE RetailDW;
+
+SELECT DATABASE() AS CurrentDatabase;
